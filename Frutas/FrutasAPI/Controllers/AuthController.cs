688e175df4fbca7dcfb62a7c13f6b78c14f23ec7
Elias Oliveira <elias_albuquerque@yahoo.com.br>
2024-09-26 19:10:08 -0300

    frutas

---
+++ Frutas/FrutasAPI/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] UserLogin userLogin)
+    {
+        if (userLogin.Username == "admin" && userLogin.Password == "123")
+        {
+            var token = GenerateJwtToken();
+            return Ok(new { token });
+        }
+        return Unauthorized();
+    }
+
+    private string GenerateJwtToken()
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpireMinutes"])),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
+
+public class UserLogin
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ Frutas/FrutasAPI/Controllers/FrutasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Authorize,
+ Route("api/[controller]")]
+public class FrutasController : ControllerBase
+{
+    private static List<string> frutas = new List<string> { "Maçã", "Banana", "Laranja" };
+
+    [HttpGet]
+    public IActionResult GetFrutas()
+    {
+        return Ok(frutas);
+    }
+
+    [HttpPost]
+    public IActionResult AddFruta([FromBody] string fruta)
+    {
+        frutas.Add(fruta);
+        return Ok(frutas);
+    }
+}
